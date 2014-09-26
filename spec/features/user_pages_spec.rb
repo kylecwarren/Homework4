d949817c886ead4abcda4a09cbdb6bfc0b8f2128
Warren Kyle C <x53706@usmawkddth17614.eecs.usma.edu>
2014-09-25 21:51:40 -0400

    Finish Sign-up

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe "UserPages", :type => :request do
   describe "GET /user_pages" do
     it "Sign Up page exists" do
@@ -8,14 +7,31 @@ RSpec.describe "UserPages", :type => :request do
 	end
   end
 end
- 
- describe "User pages" do
 
+describe "User pages" do
 	let(:base_title) { "USMA CS450" }
+ describe "Signup page" do
+	before {visit signup_path}	
+	let(:submit) { "Create my account" }
 	
-  describe "Signup page" do
-	before {visit signup_path}
-	
+	describe "with invalid information" do
+	  it "should not create a user" do
+	    expect { click_button submit }.not_to change(User, :count)
+	  end
+	end
+
+	describe "with valid information" do
+	  before do
+	    fill_in "Name",	 with: "Example User"
+	    fill_in "Email",	 with: "user@example.com"
+	    fill_in "Password",	 with: "foobar"
+	    fill_in "Confirmation", with: "foobar"
+	end
+
+	it "should create a user" do
+	  expect { click_button submit }.to change(User, :count).by(1)
+	end
+
     it "should have the h1 'USMA CS450 Sample App'" do
        expect(page).to have_selector('h1', :text => 'Sign Up')
     end
@@ -23,6 +39,5 @@ end
 		expect(page).to have_title "#{base_title} | Sign Up"
 	end
   end
-
-
+end
 end
